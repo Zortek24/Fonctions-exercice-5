6c8f8e5f4c37aef6697e944901746dcdb8b7b387
Alan <24800alan@gmail.com>
2021-11-19 10:48:44 +0100

    Ajoutez des fichiers projet.

---
+++ Fonctions exercice 5/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fonctions_exercice_5
+{
+    class Program
+    {
+        static int abs(int n)
+        {
+            if (n < 0)
+            {
+                return -n;
+            }
+            else
+            {
+                return n;
+            }
+        }
+        static void Main(string[] args)
+        {
+            int valeur;
+
+            Console.WriteLine("saisir une valeur =");
+            valeur = int.Parse(Console.ReadLine());
+            Console.WriteLine("Sa valeur absolue est ="+ abs(valeur));
+            Console.ReadLine();
+
+        }
+    }
+}
